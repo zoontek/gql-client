9367bea58127bf08454a18035fa26187fea93a36
Mathieu Acthernoene <zoontek@gmail.com>
2026-07-30 14:30:16 +0200

    update actions

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig, devices } from "@playwright/experimental-ct-react";
 import react from "@vitejs/plugin-react";
 
+const CI = process.env["CI"] === "true" || process.env["CI"] === "1";
+
 export default defineConfig({
   testDir: "./tests/react",
   outputDir: "./tests/react/results",
   timeout: 10_000,
   fullyParallel: true,
-  forbidOnly: !!process.env["CI"],
-  retries: process.env["CI"] ? 1 : 0,
+  forbidOnly: CI,
+  retries: CI ? 1 : 0,
   reporter: [
     ["list"],
     ["html", { outputFolder: "./tests/react/report", open: "never" }],
