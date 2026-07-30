36368278f415ddadcb073d98772a5a0e0edcbacf
Mathieu Acthernoene <zoontek@gmail.com>
2026-07-30 13:57:21 +0200

    migrate to tsdown

---
+++ tsdown.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "tsdown";
+
+export default defineConfig({
+  platform: "browser",
+  entry: "./src/**",
+  outDir: "./dist",
+  deps: { skipNodeModulesBundle: true },
+  minify: false,
+  sourcemap: true,
+  target: false,
+  treeshake: false,
+  unbundle: true,
+});
