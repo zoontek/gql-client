fcddda9a12be0d034f709ed5f442ed388cb58628
Mathieu Acthernoene <zoontek@gmail.com>
2026-07-30 13:29:41 +0200

    esm only

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: { index: "src/index.ts" },
-  format: ["cjs", "esm"],
+  format: "esm",
   target: "es2019",
   tsconfig: "./tsconfig.build.json",
   clean: true,
